7b637e68d399ce2e8d128347ec57b805af91e335
Azran <azran77@gmail.com>
2026-06-21 11:12:50 +0800

    Fix: return null from redirectGuestsTo so auth exceptions return 401 JSON

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
+        $middleware->redirectGuestsTo(fn () => null);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, \Illuminate\Http\Request $request) {
